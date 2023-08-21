513938e8e32497d57ffdadb251c59d7c8b682896
baobao <baopham@dosiinvn.com>
2023-08-21 12:03:23 +0700

    up

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Quốc Bảo
         </p>
         <a
           className="App-link"
